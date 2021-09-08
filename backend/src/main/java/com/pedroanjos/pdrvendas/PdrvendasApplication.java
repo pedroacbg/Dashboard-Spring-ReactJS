dc78207a2715ed47f242b83bae958dc7b29c4f19
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2021-09-08 14:13:16 -0300

    Project created

---
+++ backend/src/main/java/com/pedroanjos/pdrvendas/PdrvendasApplication.java
@@ -0,0 +1,13 @@
+package com.pedroanjos.pdrvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdrvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdrvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedroanjos/pdrvendas/PdrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroanjos.pdrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
