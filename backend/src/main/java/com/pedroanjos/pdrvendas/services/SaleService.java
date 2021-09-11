c350cfa6347a0515dce211a31a3cf9c40bf15350
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2021-09-11 09:43:45 -0300

    Group by search

--- backend/src/main/java/com/pedroanjos/pdrvendas/services/SaleService.java
+++ backend/src/main/java/com/pedroanjos/pdrvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.pedroanjos.pdrvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pedroanjos.pdrvendas.dto.SaleDTO;
+import com.pedroanjos.pdrvendas.dto.SaleSuccessDTO;
+import com.pedroanjos.pdrvendas.dto.SaleSumDTO;
 import com.pedroanjos.pdrvendas.entities.Sale;
 import com.pedroanjos.pdrvendas.repositories.SaleRepository;
 import com.pedroanjos.pdrvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerReposoritory.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
